0d95672e0a8a12f63b3b78d7847c058ddfc0659c
roman <roman-1901@mail.ru>
2022-10-19 22:59:09 +0300

    Homework3

--- src/Task8.java
+++ src/Task8.java
@@ -1,14 +1,17 @@
 public class Task8 {
     public static void main(String[] args) {
-        double Masha = 67760;
-        double Den = 83690;
-        double Kris = 76230;
-        double percent = 0.1;
-        double newMasha = Masha*percent + Masha;
-        double newDen = Den*percent + Den;
-        double newKris = Kris*percent + Kris;
-        System.out.println("Маша теперь получает "+newMasha+" рублей. Годовой доход вырос на "+(newMasha*12-Masha*12)+" рублей");
-        System.out.println("Денис теперь получает "+newDen+" рублей. Годовой доход вырос на "+(newDen*12-Den*12)+" рублей");
-        System.out.println("Кристина теперь получает "+newKris+" рублей. Годовой доход вырос на "+(newKris*12-Kris*12)+" рублей");
+        int Mary = 67760;
+        int Den = 83690;
+        int Kris = 76230;
+        int percent = 10;
+        double newMary = Mary/100.0*percent + Mary;
+        double yearMary = (newMary-Mary)*12;
+        double newDen = Den/100.0*percent + Den;
+        double yearDen = (newDen-Den)*12;
+        double newKris = Kris/100.0*percent + Kris;
+        double yearKris = (newKris-Kris)*12;
+        System.out.println("Маша теперь получает "+(int)newMary+" рублей. Годовой доход вырос на "+(int)yearMary+" рублей");
+        System.out.println("Денис теперь получает "+(int)newDen+" рублей. Годовой доход вырос на "+(int)yearDen+" рублей");
+        System.out.println("Кристина теперь получает "+(int)newKris+" рублей. Годовой доход вырос на "+(int)yearKris+" рублей");
     }
 }
